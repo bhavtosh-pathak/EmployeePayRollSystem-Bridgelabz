34accf7c5836694e0b785a60843bcd42873efb18
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2026-02-18 14:22:44 +0530

    day 2 code

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express=require('express');
+const fs=require('fs');
 const readfile = require('./modules/fileHandler');
 const app=express();
+app.use(express.static('public'));
+app.set("view engine","ejs");
 app.get('/',(req,res)=>{
-    console.log(readfile());
-    
+    fs.readFile('./modules/employees.json','utf-8',(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        const employees=JSON.parse(data);
+        res.render('index',{employees});
+    })
 })
 const port=3000;
 app.listen(port,()=>{
